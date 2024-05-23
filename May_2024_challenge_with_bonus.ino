69ca58d58a371ef208ece056a0fabf2df1c41f80
Brian P. Cox <32041112+retrobrian@users.noreply.github.com>
2024-05-23 06:50:06 -0700

    Create May_2024_challenge_with_bonus.ino

---
+++ May_2024_challenge_with_bonus.ino
@@ -0,0 +1,51 @@
+/* May 2024 Challenge - with bonus challenge
+  Write a sketch and build a circuit that lights up four LEDs from four different digital pins.
+  LED 1 should be red. It should light up for one second, then turn off.
+  LED 2 should be green. It should light up and wait for 5 seconds before going on to the next step.
+  LED 3 should be red. It should light up for 7 seconds and then turn off.
+  Bonus:
+  LED 4 should be green. It should blink five times at 1 second of and on before restarting the loop
+
+  Then turn all LEDs off for 30 seconds and then restart.
+  Notes: Use whichever GPIO pins you want to use. Don’t forget your resistors! 
+
+  Written by Brian Cox May 2024
+*/
+
+const int LED1 = 3;
+const int LED2 = 4;
+const int LED3 = 5;
+const int LED4 = 6;
+
+void setup() {
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
+}
+
+void loop() {
+  //Power up LED1 for one second then turn off
+  digitalWrite(LED1, HIGH);
+  delay(1000);
+  digitalWrite(LED1, LOW);
+  //Power up LED2, then wait for five seconds before continuing
+  digitalWrite(LED2, HIGH);
+  delay(5000);
+  //Power up LED3 for seven seconds then turn off
+  digitalWrite(LED3, HIGH);
+  delay(7000);
+  digitalWrite(LED3, LOW);
+  //Blink LED 4 five times for one second each time
+  int counter = 0;
+  while counter < 5 {
+    digitalWrite(LED4, HIGH);
+    delay(1000);
+    digitalWrite(LED4, LOW);
+    counter ++
+  }
+    //Power off LED2 which was left on, as requested in the challenge description
+  digitalWrite(LED2, LOW);
+  //Wait thirty seconds then restart
+  delay(30000);
+}
